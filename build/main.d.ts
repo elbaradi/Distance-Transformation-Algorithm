b0126fd17c251957c0a7a0b290aecc2c5cfe133b
Tijmen T. El-Baradi <t.elbaradi@gmail.com>
2021-12-07 00:18:18 +0100

    cleaned main.ts

--- build/main.d.ts
+++ build/main.d.ts
@@ -1,10 +1,12 @@
-declare const CHARCODEZERO = 47;
 declare const BLACK = "0";
 declare const WHITE = "1";
-declare const CHARCODENINE = 57;
-declare const nbrMapSpecs = 2;
-declare const minMapSize = 1;
-declare const maxMapSize = 182;
+declare const CHAR_CODE_ZERO = 47;
+declare const CHAR_CODE_NINE = 57;
+declare const NBR_MAP_SPECS = 2;
+declare const MIN_MAP_SIZE = 1;
+declare const MAX_MAP_SIZE = 182;
+declare const ERR_INVALID_NBR_OF_TESTS = 401;
+declare const ERR_INVALID_MAP = 402;
 declare const Denque: any;
 declare function stringHasOnlyDigits(string: string): boolean;
 declare function getNumberOfTests(line: string): number;
@@ -14,7 +16,7 @@ declare function mapIsValid(dimensions: dimensions, lines: string[], start: numb
 declare function inputError(errno: number): never;
 declare function locateWhitePixelsInLine(line: string, deque: dequeElement[], y: number): void;
 declare function updateCurrentPixelDistanceToMap(distanceMap: number[], pixelIndex: number, pixelDistance: number): void;
-declare function calculateNeighborsDistance(currentPixel: dequeElement, deque: dequeElement[]): void;
+declare function calculateSurroundingPixelDistance(currentPixel: dequeElement, deque: dequeElement[]): void;
 declare function fillDistanceMap(distanceMap: number[], deque: any, dimensions: dimensions): void;
 declare function printDistanceMap(distanceMap: number[], width: number): void;
 interface dimensions {
--- build/main.js
+++ build/main.js
@@ -1,21 +1,22 @@
 "use strict";
-const CHARCODEZERO = 47;
 const BLACK = '0';
 const WHITE = '1';
-const CHARCODENINE = 57;
-const nbrMapSpecs = 2;
-const minMapSize = 1;
-const maxMapSize = 182;
+const CHAR_CODE_ZERO = 47;
+const CHAR_CODE_NINE = 57;
+const NBR_MAP_SPECS = 2;
+const MIN_MAP_SIZE = 1;
+const MAX_MAP_SIZE = 182;
+const ERR_INVALID_NBR_OF_TESTS = 401;
+const ERR_INVALID_MAP = 402;
 const Denque = require("denque");
 function stringHasOnlyDigits(string) {
     const stringLength = string.length;
     for (let i = 0; i < string.length; i++) {
-        if (string.charCodeAt(i) < CHARCODEZERO || string.charCodeAt(i) > CHARCODENINE)
+        if (string.charCodeAt(i) < CHAR_CODE_ZERO || string.charCodeAt(i) > CHAR_CODE_NINE)
             return false;
     }
     return true;
 }
-// Get number of testcases
 function getNumberOfTests(line) {
     var nbrTests = 0;
     if (arrayLength > 0 && line.length > 0 && stringHasOnlyDigits(line))
@@ -24,15 +25,15 @@ function getNumberOfTests(line) {
 }
 function setMapDimensionsIfValid(dimensions, dimensionsString) {
     const mapSpecs = dimensionsString.split(' ');
-    const mapSpecsAreCorrectlyFormatted = (mapSpecs.length == nbrMapSpecs && stringHasOnlyDigits(mapSpecs[0]) && stringHasOnlyDigits(mapSpecs[1]));
+    const mapSpecsAreCorrectlyFormatted = (mapSpecs.length == NBR_MAP_SPECS && stringHasOnlyDigits(mapSpecs[0]) && stringHasOnlyDigits(mapSpecs[1]));
     if (mapSpecsAreCorrectlyFormatted) {
         dimensions.height = parseInt(mapSpecs[0]);
         dimensions.width = parseInt(mapSpecs[1]);
     }
     else
         return false;
-    const isValidHeight = (dimensions.height >= minMapSize && dimensions.height <= maxMapSize);
-    const isValidWidth = (dimensions.width >= minMapSize && dimensions.width <= maxMapSize);
+    const isValidHeight = (dimensions.height >= MIN_MAP_SIZE && dimensions.height <= MAX_MAP_SIZE);
+    const isValidWidth = (dimensions.width >= MIN_MAP_SIZE && dimensions.width <= MAX_MAP_SIZE);
     if (isValidHeight && isValidWidth)
         return true;
     else
@@ -53,14 +54,13 @@ function isValidMapLine(line, dimensions) {
 function mapIsValid(dimensions, lines, start) {
     const mapHasCorrectNumberOfLines = (arrayLength > start + dimensions.height);
     if (!mapHasCorrectNumberOfLines)
-        inputError(1);
+        inputError(ERR_INVALID_MAP);
     const mapEndsWithEOFOrEmptyLine = ((arrayLength == start + dimensions.height + 1) || (lines[start + dimensions.height + 1].length == 0));
     if (!mapEndsWithEOFOrEmptyLine)
-        inputError(2);
+        inputError(ERR_INVALID_MAP);
     for (let i = start + 1; i < start + dimensions.height; i++) {
         if (!isValidMapLine(lines[i], dimensions)) {
-            console.log(lines[i] + '\n');
-            inputError(3);
+            inputError(ERR_INVALID_MAP);
         }
     }
     return true;
@@ -80,7 +80,7 @@ function locateWhitePixelsInLine(line, deque, y) {
 function updateCurrentPixelDistanceToMap(distanceMap, pixelIndex, pixelDistance) {
     distanceMap[pixelIndex] = pixelDistance;
 }
-function calculateNeighborsDistance(currentPixel, deque) {
+function calculateSurroundingPixelDistance(currentPixel, deque) {
     if (currentPixel.west)
         deque.push({ 'distance': currentPixel.distance + 1, 'x': currentPixel.x - 1, 'y': currentPixel.y, 'west': true, 'east': false, 'north': true, 'south': true });
     if (currentPixel.east)
@@ -100,7 +100,7 @@ function fillDistanceMap(distanceMap, deque, dimensions) {
             deque.shift();
         else {
             updateCurrentPixelDistanceToMap(distanceMap, currentPixelIndex, currentPixel.distance);
-            calculateNeighborsDistance(currentPixel, deque);
+            calculateSurroundingPixelDistance(currentPixel, deque);
             deque.shift();
         }
     }
@@ -126,141 +126,24 @@ const arrayLength = inputStringArray.length;
 var index = 0;
 const nbrTests = getNumberOfTests(inputStringArray[index]);
 if (nbrTests == 0 || nbrTests > 1000)
-    inputError(4);
+    inputError(ERR_INVALID_NBR_OF_TESTS);
 var dimensions = { 'height': 0, 'width': 0 };
 for (let i = 0; i < nbrTests; i++) {
     (index < arrayLength - 1) ? index++ : inputError(5);
     if (setMapDimensionsIfValid(dimensions, inputStringArray[index]) && mapIsValid(dimensions, inputStringArray, index)) {
         var distanceMap = new Array(dimensions.height * dimensions.width).fill(-1);
-        var deque = new Denque(); // deque: dequeElement[]
+        var deque = new Denque(); // why can't we do deque: dequeElement[]?
         for (let j = 1; j <= dimensions.height; j++) {
             locateWhitePixelsInLine(inputStringArray[index + j], deque, j);
         }
         var noWhitePixels = deque.isEmpty();
         if (noWhitePixels)
-            inputError(6);
+            inputError(ERR_INVALID_MAP);
         fillDistanceMap(distanceMap, deque, dimensions);
         printDistanceMap(distanceMap, dimensions.width);
         index += dimensions.height;
     }
+    else {
+        inputError(ERR_INVALID_MAP);
+    }
 }
-// import getStdin from 'get-stdin';
-// async function main() {
-//     var value = await Promise.resolve('Hey there');
-//     console.log('inside: ' + value);
-//     return value;
-//     console.log(await getStdin());
-//     return 0;
-// }
-// var nbrTestcases : int 
-// var readline = require('readline');
-// var rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-// var i = 0;
-// while(i < 10) {
-// rl.question("", function(answer: string) {
-//    console.log("Hello " + answer);
-//    rl.close();
-// });
-// i++;
-// }
-// import { any, count } from 'dotless';
-// import {
-//     buildGrid,
-//     cellNeighbors,
-//     getGrid,
-//     Grid,
-//     gridBoundChecker,
-//     GridLocation,
-//     neighborsDelta,
-// } from './util';
-// const readline : require('readline');
-// const rl = readline.createInterface({input : process.stdin,
-//                                     output : process.stdout})
-// const WHITE = '1';
-// const BLACK = '0';
-// const OCCUPIED = '*';
-// type STATE = '1' | '0' | '*';
-// type Layout = Grid<STATE>;
-// type Location = GridLocation;
-// type AdjacentFinder = (l: Location) => Location[];
-// type AdjacentStrategy = (layout: Layout) => AdjacentFinder;
-// const isSeat = (c: STATE) => c !== FLOOR;
-// const isOccupied = (c: STATE) => c === OCCUPIED;
-// const simple: AdjacentStrategy = cellNeighbors;
-// const deep: AdjacentStrategy = (layout: Layout) => {
-//     const withInBound = gridBoundChecker(layout);
-//     return ([r, c]: Location) => {
-//         const result: Location[] = [];
-//         for (const [dr, dc] of neighborsDelta()) {
-//             let [nr, nc] = [r + dr, c + dc];
-//             while (withInBound([nr, nc])) {
-//                 if (isSeat(layout[nr][nc])) {
-//                     result.push([nr, nc]);
-//                     break;
-//                 }
-//                 [nr, nc] = [nr + dr, nc + dc];
-//             }
-//         }
-//         return result;
-//     };
-// };
-// const memoize = (base: AdjacentFinder): AdjacentFinder => {
-//     const memory = new Map<string, Location[]>();
-//     return ([r, c]: Location): Location[] => {
-//         const key = `${r}.${c}`;
-//         if (memory.has(key)) {
-//             return memory.get(key) as Location[];
-//         }
-//         const result = base([r, c]);
-//         memory.set(key, result);
-//         return result;
-//     };
-// };
-// const getNextState = (
-//     layout: Layout,
-//     adjacentSeatsOf: AdjacentFinder,
-//     t: number
-// ): [Layout, number] => {
-//     const rc = layout.length;
-//     const cc = layout[0].length;
-//     const occupied = ([r, c]: Location) => isOccupied(layout[r][c]);
-//     const filled = (l: Location) => count(occupied)(adjacentSeatsOf(l));
-//     const anyFilled = (l: Location) => any(occupied)(adjacentSeatsOf(l));
-//     const stateChange = {
-//         [FLOOR]: (_l: Location) => FLOOR as STATE,
-//         [EMPTY]: (l: Location) => (anyFilled(l) ? EMPTY : OCCUPIED),
-//         [OCCUPIED]: (l: Location) => (filled(l) >= t ? EMPTY : OCCUPIED),
-//     };
-//     let occupiedCount = 0;
-//     const nextState = buildGrid(rc, cc, ([ri, ci]) => {
-//         const current = layout[ri][ci];
-//         const next = stateChange[current]([ri, ci]);
-//         if (next === OCCUPIED) {
-//             occupiedCount = occupiedCount + 1;
-//         }
-//         return next;
-//     });
-//     return [nextState, occupiedCount];
-// };
-// const solve = (layout: Layout, strategy: AdjacentStrategy = simple, t = 4) => {
-//     let current = layout,
-//         lastOccupiedCount = -1,
-//         occupiedCount = 0;
-//     const finder = memoize(strategy(layout));
-//     while (lastOccupiedCount !== occupiedCount) {
-//         lastOccupiedCount = occupiedCount;
-//         [current, occupiedCount] = getNextState(current, finder, t);
-//     }
-//     return occupiedCount;
-// };
-// test('11', () => {
-//     const testInput01 = getGrid('11-test') as Layout;
-//     const input = getGrid('11') as Layout;
-//     expect(solve(testInput01)).toEqual(37);
-//     expect(solve(input)).toEqual(2296);
-//     expect(solve(testInput01, deep, 5)).toEqual(26);
-//     expect(solve(input, deep, 5)).toEqual(2089);
-// });
